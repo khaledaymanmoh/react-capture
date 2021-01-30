b3ff3babdaffa45ff9279eb2f57cb17791de6f2b
Khaled Ayman <khaledaymanmoh@gmail.com>
2021-01-30 16:56:33 +0200

    first commit

---
+++ src/components/FaqSection.js
@@ -0,0 +1,87 @@
+import React from "react";
+import styled from "styled-components";
+import { About } from "../styles";
+import Toggle from "./Toggle";
+import { AnimateSharedLayout } from "framer-motion";
+import { useScroll } from "./useScroll";
+import { scrollReveal } from "../animation"
+
+const FaqSection = () => {
+  const [element, controls] = useScroll();
+  return (
+    <Faq variants={scrollReveal} ref={element} animate={controls}>
+      <h2>
+        Any Question <span>FAQ</span>
+      </h2>
+      <AnimateSharedLayout>
+        <Toggle title="How do I start">
+          <div className='answer'>
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+              Laboriosam, voluptatum.
+          </p>
+          </div>
+        </Toggle>
+
+        <Toggle title="Daily schedule?">
+          <div className='answer'>
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+              Laboriosam, voluptatum.
+          </p>
+          </div>
+        </Toggle>
+
+        <Toggle title="Different payement methods?">
+          <div className='answer'>
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+              Laboriosam, voluptatum.
+          </p>
+          </div>
+        </Toggle>
+        <Toggle title="What products?">
+          <div className='answer'>
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+              Laboriosam, voluptatum.
+          </p>
+          </div>
+        </Toggle>
+      </AnimateSharedLayout>
+    </Faq>
+  );
+};
+
+const Faq = styled(About)`
+  display: block;
+  span {
+    display: block;
+  }
+  h2 {
+    padding-bottom: 2rem;
+    font-weight: lighter;
+  }
+  .faq-line {
+    background: #cccccc;
+    height: 0.2rem;
+    margin: 2rem 0rem;
+    width: 100%;
+  }
+  .question {
+    padding: 3rem 0rem;
+    cursor: pointer;
+  }
+  .answer {
+    padding: 2rem 0rem;
+    p {
+      padding: 1rem 0rem;
+    }
+  }
+`;
+
+export default FaqSection;
